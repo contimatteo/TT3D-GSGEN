a50adfdb94e82fa851782b4ea95e84892a261b2e
Matteo Conti <contimatteo1505@gmail.com>
2024-01-31 18:00:21 +0100

    WIP

--- gs/backend.py
+++ gs/backend.py
@@ -7,7 +7,7 @@ _src_path = os.path.dirname(os.path.abspath(__file__))
 
 nvcc_flags = [
     "-O3",
-    "-std=c++14",
+    "-std=c++17",  # "-std=c++14",
     "-U__CUDA_NO_HALF_OPERATORS__",
     "-U__CUDA_NO_HALF_CONVERSIONS__",
     "-U__CUDA_NO_HALF2_OPERATORS__",
@@ -16,7 +16,7 @@ nvcc_flags = [
 include_dirs = torch.utils.cpp_extension.include_paths()
 
 if os.name == "posix":
-    c_flags = ["-O3", "-std=c++14"]
+    c_flags = ["-O3", "-std=c++17"]  # ["-O3", "-std=c++14"]
 elif os.name == "nt":
     c_flags = ["/O2", "/std:c++17"]
 
@@ -27,9 +27,8 @@ elif os.name == "nt":
         for edition in ["Enterprise", "Professional", "BuildTools", "Community"]:
             paths = sorted(
                 glob.glob(
-                    r"C:\\Program Files (x86)\\Microsoft Visual Studio\\*\\%s\\VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64"
-                    % edition
-                ),
+                    r"C:\\Program Files (x86)\\Microsoft Visual Studio\\*\\%s\\VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64" %
+                    edition),
                 reverse=True,
             )
             if paths:
@@ -39,27 +38,20 @@ elif os.name == "nt":
     if os.system("where cl.exe >nul 2>nul") != 0:
         cl_path = find_cl_path()
         if cl_path is None:
-            raise RuntimeError(
-                "Could not locate a supported Microsoft Visual C++ installation"
-            )
+            raise RuntimeError("Could not locate a supported Microsoft Visual C++ installation")
         os.environ["PATH"] += ";" + cl_path
 
 include_dirs.append(os.path.join(_src_path, "src", "include"))
-include_files = list(
-    map(lambda x: str(x), Path(os.path.join(_src_path, "src", "include")).iterdir())
-)
+include_files = list(map(lambda x: str(x), Path(os.path.join(_src_path, "src", "include")).iterdir()))
 
 _backend = load(
     name="_gs",
     extra_cflags=c_flags,
     extra_cuda_cflags=nvcc_flags,
-    sources=[
-        os.path.join(_src_path, "src", f)
-        for f in [
-            "render.cu",
-            "bindings.cpp",
-        ]
-    ],
+    sources=[os.path.join(_src_path, "src", f) for f in [
+        "render.cu",
+        "bindings.cpp",
+    ]],
     extra_include_paths=include_dirs,
     verbose=True,
 )
--- gs/setup.py
+++ gs/setup.py
@@ -8,7 +8,7 @@ _src_path = os.path.dirname(os.path.abspath(__file__))
 
 nvcc_flags = [
     "-O3",
-    "-std=c++14",
+    "-std=c++17",  # "-std=c++14",
     # "-G",
     # "-g",
     # "-lineinfo",
@@ -29,20 +29,18 @@ include_dirs = torch.utils.cpp_extension.include_paths()
 print(f"cub_home: {cub_home}")
 
 if cub_home is None:
-    warnings.warn(
-        "The environment variable `CUB_HOME` was not found."
-        "Installation will fail if your system CUDA toolkit version is less than 11."
-        "NVIDIA CUB can be downloaded "
-        "from `https://github.com/NVIDIA/cub/releases`. You can unpack "
-        "it to a location of your choice and set the environment variable "
-        "`CUB_HOME` to the folder containing the `CMakeListst.txt` file."
-    )
+    warnings.warn("The environment variable `CUB_HOME` was not found."
+                  "Installation will fail if your system CUDA toolkit version is less than 11."
+                  "NVIDIA CUB can be downloaded "
+                  "from `https://github.com/NVIDIA/cub/releases`. You can unpack "
+                  "it to a location of your choice and set the environment variable "
+                  "`CUB_HOME` to the folder containing the `CMakeListst.txt` file.")
 else:
     include_dirs.append(os.path.realpath(cub_home).replace("\\ ", " "))
     pass
 
 if os.name == "posix":
-    c_flags = ["-O3", "-std=c++14", "-g"]
+    c_flags = ["-O3", "-std=c++17", "-g"]  # ["-O3", "-std=c++14", "-g"]
 elif os.name == "nt":
     nvcc_flags.append("-DWIN32_LEAN_AND_MEAN")
     c_flags = ["/O2", "/std:c++17"]
@@ -54,9 +52,8 @@ elif os.name == "nt":
         for edition in ["Enterprise", "Professional", "BuildTools", "Community"]:
             paths = sorted(
                 glob.glob(
-                    r"C:\\Program Files (x86)\\Microsoft Visual Studio\\*\\%s\\VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64"
-                    % edition
-                ),
+                    r"C:\\Program Files (x86)\\Microsoft Visual Studio\\*\\%s\\VC\\Tools\\MSVC\\*\\bin\\Hostx64\\x64" %
+                    edition),
                 reverse=True,
             )
             if paths:
@@ -66,9 +63,7 @@ elif os.name == "nt":
     if os.system("where cl.exe >nul 2>nul") != 0:
         cl_path = find_cl_path()
         if cl_path is None:
-            raise RuntimeError(
-                "Could not locate a supported Microsoft Visual C++ installation"
-            )
+            raise RuntimeError("Could not locate a supported Microsoft Visual C++ installation")
         os.environ["PATH"] += ";" + cl_path
 
 include_dirs.append(os.path.join(_src_path, "src", "include"))
@@ -78,13 +73,10 @@ setup(
     ext_modules=[
         CUDAExtension(
             name="_gs",  # extension name, import this to use CUDA API
-            sources=[
-                os.path.join(_src_path, "src", f)
-                for f in [
-                    "render.cu",
-                    "bindings.cpp",
-                ]
-            ],
+            sources=[os.path.join(_src_path, "src", f) for f in [
+                "render.cu",
+                "bindings.cpp",
+            ]],
             extra_compile_args={
                 "cxx": c_flags,
                 "nvcc": nvcc_flags,
