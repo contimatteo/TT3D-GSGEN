fde55ec28b36cbd3708980aed730b647c93e99fa
Matteo Conti <contimatteo1505@gmail.com>
2024-02-01 13:18:41 +0100

    WIP

--- export.sh
+++ export.sh
@@ -5,13 +5,13 @@ exit 0
 
 
 ### to ply
-CUDA_VISIBLE_DEVICES=2 PYTHONPATH="." python utils/export.py \
+CUDA_VISIBLE_DEVICES=2 PYTHONPATH="/home/mconti/TT3D/code/modules/GSGEN/" python utils/export.py \
   <your_ckpt> \
   --type ply
 
 
 ### to mesh
-CUDA_VISIBLE_DEVICES=2 PYTHONPATH="." python utils/export.py \
+CUDA_VISIBLE_DEVICES=2 PYTHONPATH="/home/mconti/TT3D/code/modules/GSGEN/" python utils/export.py \
   <your_ckpt> \
   --type mesh\
   --batch_size 65536 \
