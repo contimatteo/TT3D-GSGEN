c8fe6c1008ff28aa2bd333837d31449b334114b8
Matteo Conti <contimatteo1505@gmail.com>
2024-02-01 13:19:33 +0100

    WIP

--- export.sh
+++ export.sh
@@ -6,13 +6,13 @@ exit 0
 
 ### to ply
 CUDA_VISIBLE_DEVICES=2 PYTHONPATH="/home/mconti/TT3D/code/modules/GSGEN/" python utils/export.py \
-  <your_ckpt> \
+  "./checkpoints/a_high_quality_photo_of_a_corgi/2024-02-01/130952/ckpts/step_0.pt" \
   --type ply
 
 
 ### to mesh
 CUDA_VISIBLE_DEVICES=2 PYTHONPATH="/home/mconti/TT3D/code/modules/GSGEN/" python utils/export.py \
-  <your_ckpt> \
+  "./checkpoints/a_high_quality_photo_of_a_corgi/2024-02-01/130952/ckpts/step_0.pt" \
   --type mesh\
   --batch_size 65536 \
   --reso 256 \
