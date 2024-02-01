d6ed319dd5ddda69cd45a7616ad61b538b4b1c72
Matteo Conti <contimatteo1505@gmail.com>
2024-02-01 13:07:13 +0100

    WIP

--- generate.sh
+++ generate.sh
@@ -4,8 +4,8 @@ exit 0
 
 CUDA_VISIBLE_DEVICES=2 python main.py \
   --config-name="base" \
-  wandb=False \
-  batch_size=1 \
   prompt.prompt="A high quality photo of a corgi" \
-  +disable_warnings=True \
-  max_steps=200
+  batch_size=1 \
+  max_steps=200 \
+  wandb=False \
+  +disable_warnings=True
